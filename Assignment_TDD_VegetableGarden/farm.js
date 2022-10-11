b850dae2ee19861de62c1ac2dec61c558d91b3d6
Barzin <barzin.development@gmail.com>
2022-10-11 13:54:55 +0200

    Create getYieldForCrop function and test and refactor until all tests for this function pass.

--- Assignment_TDD_VegetableGarden/farm.js
+++ Assignment_TDD_VegetableGarden/farm.js
@@ -1,5 +1,9 @@
 const getYieldForPlant = plant => plant.yield;
+const getYieldForCrop = (crops) => crops.numCrops * getYieldForPlant(crops.crop);
+
+
 
 module.exports = {
     getYieldForPlant,
+    getYieldForCrop,
 }
