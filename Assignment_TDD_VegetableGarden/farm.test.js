d302f1aae8e04a4f9c43477b820f3ac0531088fa
Barzin <barzin.development@gmail.com>
2022-10-13 14:49:17 +0200

    Create test for: calculate the revenue for a crop (without environmental factors). Testresult: ReferenceError: getRevenueForCrop is not defined.

--- Assignment_TDD_VegetableGarden/farm.test.js
+++ Assignment_TDD_VegetableGarden/farm.test.js
@@ -66,7 +66,7 @@ describe("getTotalYield", () => {
 
 
 // 1. calculate the cost for a crop: getCostsForCrop.
-describe("getCostsForCrop", () => {
+/* describe("getCostsForCrop", () => {
     test("Calculate the cost of crop.", () => {
         const corn = {
             name: "corn",
@@ -112,57 +112,56 @@ describe("getCostsForCrop", () => {
         ];
         expect(getCostsForCrop({crops})).toBe(0);
     });
-});
+}); */
 
 // 2. calculate the revenue for a crop (without environmental factors): 
-//    getRevenueForCrop.
-// describe("getRevenueForCrop", () => {
-//     test("Calculate the cost of crop.", () => {
-//         const corn = {
-//             name: "corn",
-//             yield: 3,
-//             cost: 0.80,
-//             salePrice: 1.10
-//         };
-//         const crops = [
-//             { crop: corn, numCrops: 5 },
-//         ];
-//         expect(getRevenueForCrop({crops})).toBe(12);
-//     });
-    // test("Calculate the cost of different crops.", () => {
-    //     const corn = {
-    //         name: "corn",
-    //         yield: 3,
-    //         price: 0.80
-    //     };
-    //     const peas = {
-    //         name: "Peas",
-    //         yield: 2,
-    //         price: 1.12
-    //     };
-    //     const lettuce = {
-    //         name: "Lettuce",
-    //         yield: 1,
-    //         price: 0.65
-    //     };
-    //     const crops = [
-    //         { crop: corn, numCrops: 5 },
-    //         { crop: peas, numCrops: 2 },
-    //         { crop: lettuce, numCrops: 7 },
-    //     ];
-    //     expect(getCostsForCrop({crops})).toBe(21.03);
-    // });
-    // test("Calculate the cost of different crops with price set to null.", () => {
-    //     const corn = {
-    //         name: "corn",
-    //         yield: 3,
-    //         price: null
-    //     };
-    //     const crops = [
-    //         { crop: corn, numCrops: 5 },
-    //     ];
-    //     expect(getCostsForCrop({crops})).toBe(0);
-    // });
+describe("getRevenueForCrop", () => {
+    test("calculate the revenue for a crop (without environmental factors).", () => {
+        const corn = {
+            name: "corn",
+            yield: 3,
+            cost: 0.80,
+            salePrice: 1.10
+        };
+        const crops = [
+            { crop: corn, numCrops: 5 },
+        ];
+        expect(getRevenueForCrop({crops})).toBe(12);
+    });
+  /*   test("Calculate the cost of different crops.", () => {
+        const corn = {
+            name: "corn",
+            yield: 3,
+            price: 0.80
+        };
+        const peas = {
+            name: "Peas",
+            yield: 2,
+            price: 1.12
+        };
+        const lettuce = {
+            name: "Lettuce",
+            yield: 1,
+            price: 0.65
+        };
+        const crops = [
+            { crop: corn, numCrops: 5 },
+            { crop: peas, numCrops: 2 },
+            { crop: lettuce, numCrops: 7 },
+        ];
+        expect(getCostsForCrop({crops})).toBe(21.03);
+    });
+    test("Calculate the cost of different crops with price set to null.", () => {
+        const corn = {
+            name: "corn",
+            yield: 3,
+            price: null
+        };
+        const crops = [
+            { crop: corn, numCrops: 5 },
+        ];
+        expect(getCostsForCrop({crops})).toBe(0);
+    }); */
     
-// });
+});
 
