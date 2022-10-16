bbfc6176888ac9e0d66a2b1e629953912a6fc21e
Barzin <barzin.development@gmail.com>
2022-10-16 14:34:46 +0200

    Refactor and testing all previous functions and make sure all tests for all functions including getTotalProfit, pass the testcases.

--- Assignment_TDD_VegetableGarden/farm.js
+++ Assignment_TDD_VegetableGarden/farm.js
@@ -1,73 +1,43 @@
 
 // testing purposes:
 const log = console.log;
-
+// ----------------------------------------------------------------------------//
 const getYieldForPlant = plant => plant.yield;
-const getYieldForCrop = (singleCropObj) => {
-    // log(`=======================getYieldForCrop==================================`);
-    // log(`cropsObj =>`, cropsObj);
-    // log(`cropsObj =>`, cropsObj);
-    return singleCropObj.crop.yield * singleCropObj.numCrops;
-    // log(`=========================================================`);
-} 
+const getYieldForCrop = (singleCropObj) => singleCropObj.crop.yield * singleCropObj.numCrops;
 
 const getTotalYield = (cropsObj) => {
     let totalCrops = 0;
     cropsObj.crops.forEach(obj => totalCrops += obj.crop.yield * obj.numCrops);
-    return totalCrops
+    return totalCrops;
 }
 
 // calculate the cost of a crop (e.g. crop of corn, so only of one sepecies)
 const getCostsForCrop = (cropsObj) => {
     let costsForCrop = 0;
-    // cropsObj.crops.forEach(obj => costsForCrop += obj.crop.cost * getYieldForCrop(obj));
-    
-    cropsObj.crops.forEach(obj =>{
-
-        // log(`=======================COSTS==================================`);
-        
-        // log(`obj =>`, obj);
-        // log(`getYieldForCrop(obj) =>`, getYieldForCrop(obj));
-        // log(`obj.crop.cost =>`, obj.crop.cost);
-        costsForCrop += obj.crop.cost * getYieldForCrop(obj);
-        // log(`costsForCrop =>`, costsForCrop);
-        // revenueForCrop += obj.crop.salePrice * getYieldForCrop(obj);
-        // log(`=========================================================`);
-    });  
-
-    return costsForCrop;
+    cropsObj.crops.forEach(obj => costsForCrop += obj.crop.cost * getYieldForCrop(obj));  
+    return  costsForCrop;
 }
+
+// calculate the revenue of a crop (e.g. crop of corn, so only of one sepecies)
 const getRevenueForCrop = (cropsObj) => {
-    // log(`cropsObj in getRevenueForCrop ==> `,cropsObj)
     let revenueForCrop = 0;
-    cropsObj.crops.forEach(obj =>{
-        // log(`==========================REVENUE===============================`);
-        revenueForCrop += obj.crop.salePrice * getYieldForCrop(obj);
-        // log(`=========================================================`);
-    });  
+    cropsObj.crops.forEach(obj => revenueForCrop += obj.crop.salePrice * getYieldForCrop(obj));  
     return revenueForCrop;
 };
 
 const getProfitForCrop = (cropsObj) =>{
     const profitForCrop = getRevenueForCrop(cropsObj) - getCostsForCrop(cropsObj);
-    return profitForCrop < 0 ? 0 : profitForCrop; 
-    // log(`getRevenueForCrop(cropsObj) =>`, getRevenueForCrop(cropsObj));
-    // log(` getCostsForCrop(cropsObj) =>`, getCostsForCrop(cropsObj));
-    // log(` getRevenueForCrop(cropsObj) - getCostsForCrop(cropsObj) =>`, getRevenueForCrop(cropsObj) - getCostsForCrop(cropsObj));
-
+    return profitForCrop < 0 ? 0 : parseFloat(profitForCrop.toFixed(2));
 }
 
 const getTotalProfit = (cropsObj) =>{
-  
-    // cropsObj.crops.forEach(obj=>{
-        log(`==========================TOTAL PROFIT=========================`);
-        // const crops = [obj];
-        // log(`{newObj} =>`, {crops});
-        // log(`getProfitForCrop({newObj}) =>`, getProfitForCrop({crops}));
-        // log(` =>`, );
-        // log(` =>`, );
-    // })
-    log(`=========================================================`);
+    let totalProfit = 0;
+    let crops = [];
+    cropsObj.crops.forEach(obj=>{
+        crops = [obj];
+        totalProfit += getProfitForCrop({crops});
+    });
+    return parseFloat(totalProfit.toFixed(2));
 }
 
 module.exports = {
